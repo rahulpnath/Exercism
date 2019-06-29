53fedf49527f3a8c651396cd1c9501b1017f2070
Rahul P Nath <rahulpnath@gmail.com>
2019-06-30 08:52:42 +1000

    Robot Name inital attempt

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -1,8 +1,17 @@
 ﻿module RobotName
+
+let generateName () = 
+ let random = System.Random()
+ let randomChar1 = ['A'..'Z'].[random.Next(25)]
+ let randomChar2 = ['A'..'Z'].[random.Next(25)]
+ sprintf "%c%c123" 
+  randomChar1 
+  randomChar2 
+
 type RobotType = {name:string;}
 
-let mkRobot() = {name = "HK123"}
+let mkRobot() = {name = generateName()}
 
 let name robot = robot.name 
 
-let reset robot =   {name = "HG134"}
+let reset robot =   {name = generateName()}
