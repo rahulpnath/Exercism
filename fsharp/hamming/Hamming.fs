0ffad0b0ae4704f8aec8bdec094aee5b6ae22f7b
Rahul P Nath <rahulpnath@gmail.com>
2019-07-11 18:55:24 +1000

    Hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,3 +1,10 @@
 ﻿module Hamming
 
-let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
+let distance (strand1: string) (strand2: string): int option = 
+    if (strand1.Length <> strand2.Length)
+    then None
+    else
+    let items1 = strand1.ToCharArray() |> Seq.indexed
+    let items2 = strand2.ToCharArray() |> Seq.indexed
+    let length = Seq.concat [items1;items2] |> Seq.distinct |> Seq.length
+    Some (System.Math.Abs(strand1.Length - length))
