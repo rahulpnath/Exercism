4317dc84e5bdb715821890712b5da1011b846587
Rahul P Nath <rahulpnath@gmail.com>
2019-07-13 05:13:51 +1000

    Hamming use map2

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -4,7 +4,4 @@ let distance (strand1: string) (strand2: string): int option =
     if (strand1.Length <> strand2.Length)
     then None
     else
-    let items1 = strand1.ToCharArray() |> Seq.indexed
-    let items2 = strand2.ToCharArray() |> Seq.indexed
-    let length = Seq.concat [items1;items2] |> Seq.distinct |> Seq.length
-    Some (System.Math.Abs(strand1.Length - length))
+    Some (Seq.map2 (<>) strand1 strand2 |> Seq.filter id |> Seq.length)
--- fsharp/hamming/HammingTest.fs
+++ fsharp/hamming/HammingTest.fs
@@ -1,4 +1,4 @@
-// This file was auto-generated based on version 2.1.0 of the canonical data.
+// This file was auto-generated based on version 2.3.0 of the canonical data.
 
 module HammingTest
 
@@ -12,51 +12,19 @@ let ``Empty strands`` () =
     distance "" "" |> should equal (Some 0)
 
 [<Fact>]
-let ``Identical strands`` () =
+let ``Single letter identical strands`` () =
     distance "A" "A" |> should equal (Some 0)
 
 [<Fact>]
+let ``Single letter different strands`` () =
+    distance "G" "T" |> should equal (Some 1)
+
+[<Fact>]
 let ``Long identical strands`` () =
-    distance "GGACTGA" "GGACTGA" |> should equal (Some 0)
+    distance "GGACTGAAATCTG" "GGACTGAAATCTG" |> should equal (Some 0)
 
 [<Fact>]
-let ``Complete distance in single nucleotide strands`` () =
-    distance "A" "G" |> should equal (Some 1)
-
-[<Fact>]
-let ``Complete distance in small strands`` () =
-    distance "AG" "CT" |> should equal (Some 2)
-
-[<Fact>]
-let ``Small distance in small strands`` () =
-    distance "AT" "CT" |> should equal (Some 1)
-
-[<Fact>]
-let ``Small distance`` () =
-    distance "GGACG" "GGTCG" |> should equal (Some 1)
-
-[<Fact>]
-let ``Small distance in long strands`` () =
-    distance "ACCAGGG" "ACTATGG" |> should equal (Some 2)
-
-[<Fact>]
-let ``Non-unique character in first strand`` () =
-    distance "AAG" "AAA" |> should equal (Some 1)
-
-[<Fact>]
-let ``Non-unique character in second strand`` () =
-    distance "AAA" "AAG" |> should equal (Some 1)
-
-[<Fact>]
-let ``Same nucleotides in different positions`` () =
-    distance "TAG" "GAT" |> should equal (Some 2)
-
-[<Fact>]
-let ``Large distance`` () =
-    distance "GATACA" "GCATAA" |> should equal (Some 4)
-
-[<Fact>]
-let ``Large distance in off-by-one strand`` () =
+let ``Long different strands`` () =
     distance "GGACGGATTCTG" "AGGACGGATTCT" |> should equal (Some 9)
 
 [<Fact>]
@@ -66,4 +34,12 @@ let ``Disallow first strand longer`` () =
 [<Fact>]
 let ``Disallow second strand longer`` () =
     distance "ATA" "AGTG" |> should equal None
+
+[<Fact>]
+let ``Disallow left empty strand`` () =
+    distance "" "G" |> should equal None
+
+[<Fact>]
+let ``Disallow right empty strand`` () =
+    distance "G" "" |> should equal None
 
