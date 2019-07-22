66779199609b21bd481096ba769bbfc0fd17a461
Rahul P Nath <rahulpnath@gmail.com>
2019-07-23 04:57:17 +1000

    Raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,6 @@
+﻿module Raindrops
+let convert (number: int): string = 
+    [(3,"Pling");(5,"Plang");(7,"Plong")] 
+    |> List.filter (fun (i, _) -> number % i = 0)
+    |> List.fold (fun acc (_,t) -> acc + t) ""
+    |> fun x -> if x = "" then string number else x
