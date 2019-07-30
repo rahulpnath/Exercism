870ed3b6e9a383bf8d7a4bc0823e95de415e8bef
Rahul P Nath <rahulpnath@gmail.com>
2019-07-31 05:26:59 +1000

    Grade school initial

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,18 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School = 
+    match school.TryFind(grade) with
+    | None -> school.Add(grade, [student])
+    | Some students -> school.Add(grade, (student :: students) |> List.sort)
+
+let roster (school: School): string list = 
+    school |> Map.toSeq |> Seq.fold (fun acc (_, students) -> acc @ students) []
+
+let grade (number: int) (school: School): string list = 
+    match school.TryFind number with
+    | None -> []
+    | Some students -> students
