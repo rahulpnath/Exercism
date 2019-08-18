5f08e25503a3e8d5d50ba9eb0f1f9d4d2b5e425a
Rahul P Nath <rahulpnath@gmail.com>
2019-08-19 05:11:58 +1000

    Kindergarten garden

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,17 @@
+﻿module KindergartenGarden
+open System
+
+type Plant = Grass = 'G' | Clover = 'C' | Radishes = 'R' | Violets = 'V'
+let toPlants (plantCodes:string) : Plant [] = 
+    plantCodes.ToCharArray()
+    |> Array.map (LanguagePrimitives.EnumOfValue)
+
+let children = ["Alice";"Bob";"Charlie";"David";"Eve";"Fred";"Ginny";"Harriet";"Ileana";"Joseph";"Kincaid";"Larry"]
+let indexOfChild child = children |> List.findIndex ((=) child)
+
+let plants (diagram: string) student =
+    let childIndex = indexOfChild student
+    diagram.Split([|"\n"|], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun row -> row.[childIndex*2..childIndex*2+ 1])
+    |> Array.collect (toPlants)
+    |> Array.toList
